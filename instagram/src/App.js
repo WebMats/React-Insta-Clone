4a4c211413b0d3c92fc86bcf223a257e3b436c0e
WebMats <lorlando127@gmail.com>
2019-01-15 17:01:28 -0500

    added post search capabilities

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -8,18 +8,29 @@ import './App.css';
 
 class App extends Component {
   state = {
-    dummyData: []
+    dummyData: [],
+    searchedUser: ''
   }
 
   componentDidMount() {
     this.setState({dummyData})
+  }
+  searchUserHandler = (e) => {
+    e.preventDefault();
+    this.setState(prevState => {
+      if (prevState.searchedUser.length === 0) {
+        return this.setState({dummyData})    
+      }
+      const copiedData = [...prevState.dummyData].filter(post => post.username.includes(prevState.searchedUser))
+      return {dummyData: copiedData, searchedUser: '',}
+    })
   }
 
   render() {
     const posts = this.state.dummyData.map(post => (<PostContainer key={post.username} post={post}/>))
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar changed={(e) => this.setState({searchedUser: e.target.value})} inputValue={this.state.searchedUser} searchUser={this.searchUserHandler} />
         {posts}
       </div>
     );
