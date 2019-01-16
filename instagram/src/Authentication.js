284dbd7fd8d6e14d49f27ecc3ee4c39082fea48b
WebMats <lorlando127@gmail.com>
2019-01-16 18:33:01 -0500

    added styling for login-page

--- instagram/src/Authentication.js
+++ instagram/src/Authentication.js
@@ -7,11 +7,12 @@ const authentication = App => Login => {
             super();
             this.state = {
                 loggedIn: false,
-                userName: ''
+                userName: '',
+                password: '',
             }
         }
         componentDidMount() {
-            if (localStorage.getItem('username') !== null) {
+            if (localStorage.getItem('username') !== null && localStorage.getItem('username').length !== 0) {
                 this.setState({loggedIn: true, userName: localStorage.getItem('username')})
             }
         }
@@ -19,7 +20,13 @@ const authentication = App => Login => {
             localStorage.setItem('username', this.state.userName);
           }
         render() {
-            return this.state.loggedIn ? <App user={this.state.userName} /> : <Login changed={(e) => this.setState({userName: e.target.value})} inputValue={this.state.userName} submitCredentials={this.loginHandler} />;
+            return this.state.loggedIn ? <App user={this.state.userName} /> : <Login 
+                                                                                changed={(e) => this.setState({userName: e.target.value})} 
+                                                                                inputValue={this.state.userName} 
+                                                                                submitCredentials={this.loginHandler} 
+                                                                                inputPassword={this.state.password}
+                                                                                passwordChange={(e) => this.setState({password: e.target.value})}
+                                                                            />;
         }
     }
 }
--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import './Login.css';
+
+
 const login = (props) => {
 
+        const labelUsernameClassName = props.inputValue ? "LabelUsername Moved" : "LabelUsername";
+        const labelPasswordClassName = props.inputPassword ? "LabelUsername Moved" : "LabelUsername";
+
         return (
-                        <form onSubmit={props.submitCredentials} autoComplete="off">
-                                <label>Username</label>
-                                <input value={props.inputValue} onChange={props.changed} name="username" placeholder="Username" />
-
-                                <label>Password</label>
-                                <input type="password" name="password" placeholder="Password" />
-
-                                <button className="LoginButton">Login</button>
-                        </form>
+                <form className="LoginForm" onSubmit={props.submitCredentials} autoComplete="off">
+                        <h2>Login</h2>
+                        <div className="InputHolder">
+                                <label className={labelUsernameClassName}>Username</label>
+                                <input value={props.inputValue} onChange={props.changed} name="username" placeholder="Username" autoFocus/>
+                        </div>
+                        <div className="InputHolder">
+                                <label className={labelPasswordClassName}>Password</label>
+                                <input value={props.inputPassword} onChange={props.passwordChange} type="password" name="password" placeholder="Password" />
+                        </div>
+                        <button className="LoginButton">Login</button>
+                </form>
         )
 }
 
